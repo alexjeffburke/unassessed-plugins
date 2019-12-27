baf14d0ddb78f37bfab5352f8f29899a91886ae3
Alex J Burke <alex@alexjeffburke.com>
2019-12-27 15:57:04 +0100

    Implement documentation publishing for all plugins.

---
+++ scripts/combineDocs.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const fs = require("fs-extra");
+const path = require("path");
+
+const DOCS_DIR = path.join(__dirname, "..", "documentation");
+const PACKAGES_DIR = path.join(__dirname, "..", "packages");
+
+fs.removeSync(DOCS_DIR);
+fs.mkdirpSync(DOCS_DIR);
+
+for (const dir of fs.readdirSync(PACKAGES_DIR)) {
+  if (dir.startsWith(".")) continue;
+  const packageDocsPath = path.join(PACKAGES_DIR, dir, "documentation");
+  const dirWithoutSuffix = dir.replace("unassessed-plugin-", "");
+  const combinedDocsPath = path.join(DOCS_DIR, dirWithoutSuffix);
+  fs.emptyDirSync(combinedDocsPath);
+  fs.copySync(packageDocsPath, combinedDocsPath);
+}
