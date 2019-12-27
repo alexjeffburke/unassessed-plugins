b56c9d97d00ac88226c598efedfca2fac7dc60b7
Alex J Burke <alex@alexjeffburke.com>
2019-12-27 16:38:26 +0100

    Persist .nojekyll in the combined documentation folder.
    Without this it is removed by gh-pages which causes serving of the
    dot prefixed typedoc file to break.

--- scripts/combineDocs.js
+++ scripts/combineDocs.js
@@ -6,9 +6,6 @@ const path = require("path");
 const DOCS_DIR = path.join(__dirname, "..", "documentation");
 const PACKAGES_DIR = path.join(__dirname, "..", "packages");
 
-fs.removeSync(DOCS_DIR);
-fs.mkdirpSync(DOCS_DIR);
-
 for (const dir of fs.readdirSync(PACKAGES_DIR)) {
   if (dir.startsWith(".")) continue;
   const packageDocsPath = path.join(PACKAGES_DIR, dir, "documentation");
